aee008e2a44a418ea02cb9b66d3be74afc6e74ff
ulzaalkindi <ulza.alkindi@students.amikom.ac.id>
2022-04-20 16:37:53 +0700

    feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Feed.css";
-import CreateIcon from "@mui/icons-material/Create";
+// import CreateIcon from "@mui/icons-material/Create";
+import BorderColorIcon from '@mui/icons-material/BorderColor';
 function Feed() {
-  return (
-    <div className="feed">
-      <div className="feed__inputContainer">
-        <div className="feed__input">
-          <CreateIcon />
+    return (
+        <div className="feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <BorderColorIcon />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Feed;
